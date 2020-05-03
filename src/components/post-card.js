cfe6e500b60696f7041d60c4769adfcffa4b135f
Tinashe <tinashejchiweshe@gmail.com>
2020-05-03 18:41:19 +0200

    added hover styles to post card

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
-import styled from 'styled-components'
+import styled, { css, ThemeProvider } from 'styled-components'
+
+const theme = {
+  link: '#333',
+  blogLink: '#F65286',
+}
+
+const transitionLinks = css`
+    transition: color .15s ease-out;
+    transition: border-left .15s ease-out;
+`
 
 const CardBodyContainer = styled.div`
   box-shadow: 0 2px 5px rgba(0,0,0,0.1);
@@ -10,18 +20,21 @@ const CardBodyContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-left: 10px solid slategray;
 
   &:hover {
-    top: -2px;
+    
     box-shadow: 0 4px 5px rgba(0,0,0,0.2);
+    transition: ${transitionLinks};
+    border-left: 5px solid ${theme.blogLink};
   }
 `
 
 const PostCard = ({children, ...props}) => (
+  <ThemeProvider theme={theme}>
     <CardBodyContainer {...props}>
         {children}
     </CardBodyContainer>
+  </ThemeProvider>
 )
 
 export default PostCard
